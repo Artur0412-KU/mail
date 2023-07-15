1a15052b2e5ae443fde7486b7b9c3df1516e33ac
Artur <71281020+Artur0412-ku@users.noreply.github.com>
2023-07-15 15:33:39 +0300

    Update composeemailform.js

--- src/components/composeemailform.js
+++ src/components/composeemailform.js
@@ -1,64 +1,89 @@
 import React, { useState } from 'react';
 import ReactDOM from "react-dom";
 
-export default function ComposeEmailForm() {
+export default function ComposeEmailForm({ language }) {
     const [recipient, setRecipient] = useState('');
     const [subject, setSubject] = useState('');
     const [body, setBody] = useState('');
 
-    // Handle form submission
+    useEffect(() => {
+        // Скидайте форму при зміні мови
+        setRecipient('');
+        setSubject('');
+        setBody('');
+    }, [language]);
+
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        // Create an email object
         const email = {
             recipient: recipient,
             subject: subject,
             body: body
         };
 
-        // Send the email (you can replace this with your own logic)
         sendEmail(email);
 
-        // Reset form fields
         setRecipient('');
         setSubject('');
         setBody('');
     };
 
-    // Simulate sending the email (replace this with your own logic)
-    const sendEmail = (email) => {
-        console.log('Sending email:', email);
-        // Implement your own logic to send the email
+    const sendEmail = async (email) => {
+        try {
+            const response = await fetch('your-email-send-endpoint', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(email)
+            });
+
+            if (response.ok) {
+                console.log('Email sent successfully');
+            } else {
+                console.log('Failed to send email');
+            }
+        } catch (error) {
+            console.error('Error sending email:', error);
+        }
+    };
+
+    const resetEmail = () => {
+        setRecipient('');
+        setSubject('');
+        setBody('');
     };
 
     return (
         <div className="compose-form">
-            <h2>Compose Email</h2>
+            <h2>{language === 'Українська' ? 'Створити Електронний лист' : 'Compose Email'}</h2>
             <form onSubmit={handleSubmit}>
                 <input
                     type="email"
-                    placeholder="Recipient"
+                    placeholder={language === 'Українська' ? 'Одержувач' : 'Recipient'}
                     value={recipient}
                     onChange={(e) => setRecipient(e.target.value)}
                     required
                 />
                 <input
                     type="text"
-                    placeholder="Subject"
+                    placeholder={language === 'Українська' ? 'Тема' : 'Subject'}
                     value={subject}
                     onChange={(e) => setSubject(e.target.value)}
                     required
                 />
                 <textarea
-                    placeholder="Body"
+                    placeholder={language === 'Українська' ? 'Текст повідомлення' : 'Body'}
                     value={body}
                     onChange={(e) => setBody(e.target.value)}
                     required
                 />
-                <button type="submit">Send</button>
+                <button type="submit">{language === 'Українська' ? 'Відправити' : 'Send'}</button>
+                <button type="button" onClick={resetEmail}>
+                    {language === 'Українська' ? 'Скинути' : 'Reset'}
+                </button>
             </form>
         </div>
-    )
-
+    );
 }
