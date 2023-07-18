8b6a34d6ab218aefdee0989076721bbc17ad41c2
Artur <71281020+Artur0412-ku@users.noreply.github.com>
2023-07-18 16:55:09 +0300

    Update navigation.js

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,11 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { useState } from 'react';
 
-export default function Navigation({ setActivePage }) {
+export default function Navigation({ setActivePage, language, onLanguageChange }) {
+    const [isUkrainian, setIsUkrainian] = useState(language === 'Українська');
+
+    const handleLanguageButtonClick = () => {
+        const newLanguage = isUkrainian ? 'English' : 'Українська';
+        onLanguageChange(newLanguage);
+        setIsUkrainian(!isUkrainian);
+    };
+
     return (
         <div className="btn-container">
-            <button onClick={() => setActivePage('inbox')}>Inbox</button>
-            <button onClick={() => setActivePage('sent')}>Sent</button>
+            <button onClick={() => setActivePage('inbox')}>
+                {isUkrainian ? 'Вхідні' : 'Inbox'}
+            </button>
+            <button onClick={() => setActivePage('compose')}>
+                {isUkrainian ? 'Створити' : 'Compose'}
+            </button>
+            <button className="language-btn" onClick={handleLanguageButtonClick}>{isUkrainian ? 'English' : 'Українська'  }</button>
         </div>
     );
 }
