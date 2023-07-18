cee4821d3361ee3aa2c47130d06640f4f0a7af95
Artur <71281020+Artur0412-ku@users.noreply.github.com>
2023-07-18 16:54:25 +0300

    Update Inbox.js

--- src/components/Inbox.js
+++ src/components/Inbox.js
@@ -2,27 +2,29 @@ import React, { useState, useEffect } from 'react';
 import EmailPreview from "./emailpreview";
 import { emails } from '../data/data'; // Import the emails data
 
-// Component: Inbox
-function Inbox() {
+function Inbox({ language }) {
     const [emailData, setEmailData] = useState([]);
 
     useEffect(() => {
-        setEmailData(emails); // Set the email data using the imported emails array
-    }, []);
+        setEmailData(emails);
+    }, [])
+
+    useEffect(() => {
+        document.title = language === 'Українська' ? 'Вхідні' : 'Inbox';
+    }, [language]);
 
     return (
         <div className="inbox-container">
-            <h2>Inbox</h2>
+            <h2>{language === 'Українська' ? 'Вхідні' : 'Inbox'}</h2>
             <div className="mails-container">
                 {emailData.length > 0 ? (
                     emailData.map((email) => (
-                        <EmailPreview key={email.id} email={email} />
+                        <EmailPreview key={email.id} email={email} language={language} />
                     ))
                 ) : (
-                    <p>No emails found.</p>
+                    <p>{language === 'Українська' ? 'Немає електронних листів.' : 'No emails found.'}</p>
                 )}
             </div>
-
         </div>
     );
 }
